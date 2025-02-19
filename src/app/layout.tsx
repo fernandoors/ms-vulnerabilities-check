15be78a93a8ef1306254a56edb1e470854934d55
fernandoors <fernando.ors@outlook.com>
2025-02-18 22:22:45 -0500

    config: style base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,22 @@
+import "./global.css";
 import type { Metadata } from "next";
+import { Karla, Roboto } from "next/font/google";
 
 export const metadata: Metadata = {
     title: "(Alltech - M. S.) Vulnerabilities Checker",
     description: "This is a code test for a job application",
 };
+
+const roboto = Roboto({
+    weight: ["300", "400", "500"],
+    subsets: ["latin"],
+    variable: "--font-roboto",
+});
+const karla = Karla({
+    weight: ["500", "600", "700"],
+    subsets: ["latin"],
+    variable: "--font-karla",
+});
 
 export default function RootLayout({
     children,
@@ -11,8 +24,10 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="en" className={`${roboto.variable} ${karla.variable}`}>
+            <body className="bg-gray-100 text-gray-900 antialiased">
+                {children}
+            </body>
         </html>
     );
 }
